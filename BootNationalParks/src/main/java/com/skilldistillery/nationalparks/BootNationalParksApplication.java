7431c56e77d346bc6b6662b557cb14c018509801
Ryan Harrington <harrington.ryana@gmail.com>
2019-07-08 10:09:34 -0600

    Add warfile

--- BootNationalParks/src/main/java/com/skilldistillery/nationalparks/BootNationalParksApplication.java
+++ BootNationalParks/src/main/java/com/skilldistillery/nationalparks/BootNationalParksApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.nationalparks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootNationalParksApplication {
+public class BootNationalParksApplication extends SpringBootServletInitializer {
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(BootNationalParksApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootNationalParksApplication.class, args);
