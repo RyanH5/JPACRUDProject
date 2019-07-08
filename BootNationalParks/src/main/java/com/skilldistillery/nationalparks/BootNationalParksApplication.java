6f1d75c77f621878b42e07b8b558b2c29d59ac10
Ryan Harrington <harrington.ryana@gmail.com>
2019-07-08 09:24:22 -0600

    JPA mostly set up

---
+++ BootNationalParks/src/main/java/com/skilldistillery/nationalparks/BootNationalParksApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.nationalparks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNationalParksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootNationalParksApplication.class, args);
+	}
+
+}
